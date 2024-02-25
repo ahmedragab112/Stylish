0f4ddb8d1e7273bb4410cbd3cac660b3abdc4546
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-25 03:15:56 +0200

    adding di for signup

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stylehub/config/router/routes.dart';
-import 'package:stylehub/core/api/api_manger.dart';
-import 'package:stylehub/core/api/dio_singelton.dart';
+import 'package:stylehub/core/di/injection.dart';
 import 'package:stylehub/features/home/presentation/pages/home.dart';
 import 'package:stylehub/features/login/presentation/pages/login.dart';
-import 'package:stylehub/features/signup/data/datasources/remote/remote_datasource_implementation.dart';
-import 'package:stylehub/features/signup/data/repositories/remote_repo_implmentation.dart';
-import 'package:stylehub/features/signup/domain/usecases/signup_usecase.dart';
 import 'package:stylehub/features/signup/presentation/cubit/signup_cubit.dart';
 import 'package:stylehub/features/signup/presentation/pages/singup.dart';
 
@@ -17,11 +13,7 @@ class AppRouter {
       case AppRoutes.signUp:
         return MaterialPageRoute(
           builder: (context) => BlocProvider(
-            create: (context) => SignupCubit(
-                signUpUsecase: SignUpUsecase(
-                    signUpRepo: SignUpDataRepo(
-                        remoteDataSource: RemoteDataSoucresImplementation(
-                            apiManager: ApiManager(DioFactory.getDio()))))),
+            create: (context) => locator<SignupCubit>(),
             child: const SignUp(),
           ),
         );
