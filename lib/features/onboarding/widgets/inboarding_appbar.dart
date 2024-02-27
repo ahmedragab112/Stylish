e94c91788e8990646282c79fd23103edc55f7168
ahmedragab <ar1755@fayoum.edu.eg>
2024-02-28 00:56:39 +0200

    perform blocbuilder for onboarding appbar and next prev

--- lib/features/onboarding/widgets/inboarding_appbar.dart
+++ lib/features/onboarding/widgets/inboarding_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stylehub/config/router/routes.dart';
 import 'package:stylehub/core/cache/shared_prefrences.dart';
 import 'package:stylehub/core/di/injection.dart';
@@ -6,26 +7,33 @@ import 'package:stylehub/core/extentions/extention.dart';
 import 'package:stylehub/core/utils/colors/app_color.dart';
 import 'package:stylehub/core/utils/strings/app_strings.dart';
 import 'package:stylehub/core/utils/styles/app_textstyle.dart';
+import 'package:stylehub/features/onboarding/manager/onboarding_cubit.dart';
 
 class InBoardingAppBar extends StatelessWidget {
-  const InBoardingAppBar({super.key, required this.index});
-  final int index;
+  const InBoardingAppBar({super.key, });
+  
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        RichText(
-            text: TextSpan(children: [
-          TextSpan(
-              text: '${index + 1}',
-              style: AppTextStyle.font18SemiBoldPrimeryPink
-                  .copyWith(color: Colors.black)),
-          TextSpan(
-              text: '/3',
-              style: AppTextStyle.font18SemiBoldPrimeryPink
-                  .copyWith(color: AppColor.onBoardingDescriptionGreyColor)),
-        ])),
+        BlocBuilder<OnboardingCubit, OnboardingState>(
+
+          builder: (context, state) {
+            var bloc = context.read<OnboardingCubit>();
+            return RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: '${bloc.index + 1}',
+                  style: AppTextStyle.font18SemiBoldPrimeryPink
+                      .copyWith(color: Colors.black)),
+              TextSpan(
+                  text: '/3',
+                  style: AppTextStyle.font18SemiBoldPrimeryPink.copyWith(
+                      color: AppColor.onBoardingDescriptionGreyColor)),
+            ]));
+          },
+        ),
         GestureDetector(
           onTap: () async {
             await locator<CacheHelper>()
