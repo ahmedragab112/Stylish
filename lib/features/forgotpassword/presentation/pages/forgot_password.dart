f42e364e3bff8d370e015aa22716ff03c362df54
ahmedragab <ar1755@fayoum.edu.eg>
2024-03-03 04:20:24 +0200

    finish forgot password send email ui and api integration

--- lib/features/forgotpassword/presentation/pages/forgot_password.dart
+++ lib/features/forgotpassword/presentation/pages/forgot_password.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stylehub/features/forgotpassword/manager/forgotpassword_cubit.dart';
+import 'package:stylehub/features/forgotpassword/presentation/widgets/forgot_password_body.dart';
 
 class ForgotPassword extends StatelessWidget {
   const ForgotPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var bloc = context.read<ForgotPasswordCubit>();
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await bloc.forgotPassword(email: 'vvvvvvvvvv1633232002@gmail.com');
-        },
-        child: const Icon(Icons.send_outlined),
+    return const Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(slivers: [
+          SliverToBoxAdapter(
+            child: ForgotPasswordBody(),
+          )
+        ]),
       ),
     );
   }
